764482cc36c32e99e9914570d3f5791c4ce71e9b
Guadalupe <mendoza91guada@gmail.com>
2024-03-26 15:57:17 -0300

    punteros

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
-    printf("Hola mundo");
+    printf("Hola mundo\n\n");
+    int num=10, *p;
+    p=&num;
+    printf("\nEl puntero contiene (apunta a): %d", *p);
+    printf("\nLa dirección de memoria almacenada por el puntero: %p", p);
+    printf("\nLa dirección de memoria de la variable %p", &num);
+    printf("\nLa dirección de memoria del puntero %p", &p);
+    printf("\nEl tamaño de memoria utilizado por la variable puntero: %d", sizeof(p));
+
+    return 0;
+
 }
